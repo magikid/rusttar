cde52e592a0ca341df43d5a44185d8fca9ffdbc3
Chris W Jones <chris@christopherjones.us>
2017-03-05 18:57:58 -0500

    Implementing display for header

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,35 @@
-pub fn pad_header(string: String, length: u32) -> Vec<u8> {
-    string.clone().into_bytes()
+use std::fmt;
+
+struct HeaderField {
+    value: Vec<u8>,
+    length: usize
+}
+
+impl fmt::Display for HeaderField{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let bytes_to_write = pad_field(self.value, self.length);
+
+        for byte in bytes_to_write.iter(){
+            write!(f, "{:b}", byte);
+        }
+        write!(f, "{}", "")
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use header::HeaderField;
+
+    #[test]
+    fn it_pads_the_length(){
+        let foobar_field = "foobar".to_string().as_bytes().to_vec();
+        let h = HeaderField { value: foobar_field, length: 100 };
+        let output = format!("{}", h);
+        assert_eq!(output.len(), h.length);
+    }
+}
+
+fn pad_header(value: Vec<u8>, length: usize) -> Vec<u8> {
+    let vec = Vec::with_capacity(length);
+    value.truncate(length)
 }
