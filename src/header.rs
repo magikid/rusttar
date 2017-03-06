c7d29fc2e59d998dea6722d227f517b4ae4144db
Chris W Jones <chris@christopherjones.us>
2017-03-06 16:11:48 -0500

    Truncation is proving hard
    Since I have an Vec<u8>, I can't figure out how to truncate it to the
    correct length.

--- src/header.rs
+++ src/header.rs
@@ -7,16 +7,25 @@ struct HeaderField {
 
 impl fmt::Display for HeaderField{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let ref val = self.value;
+        let mut val = self.value.clone();
         let length = self.length;
-        let pad_length = length - (val.len() * 8);
+        let value_length = val.len() * 8;
+        if length > value_length {
+            let pad_length = length - value_length;
 
-        for byte in val.iter(){
-            try!(write!(f, "{:08b}", byte));
-        }
+            for byte in val.iter(){
+                try!(write!(f, "{:08b}", byte));
+            }
 
-        for _ in 1..pad_length {
-            try!(write!(f, "0"));
+            for _ in 1..pad_length {
+                try!(write!(f, "0"));
+            }
+
+        } else {
+            println!("Truncated {} bytes", value_length - length);
+            for byte in val.iter(){
+                try!(write!(f, "{:08b}", byte));
+            }
         }
 
         write!(f, "{}", "\0")
@@ -28,8 +37,16 @@ mod tests{
     use header::HeaderField;
 
     #[test]
-    fn it_pads_the_length(){
+    fn it_pads_short_values(){
         let foobar_field = "foobar".to_string().as_bytes().to_vec();
+        let h = HeaderField { value: foobar_field, length: 100 };
+        let output = format!("{}", h);
+        assert_eq!(output.len(), h.length);
+    }
+
+    #[test]
+    fn it_truncates_long_values(){
+        let foobar_field = "foobarfoobarbaz".to_string().as_bytes().to_vec();
         let h = HeaderField { value: foobar_field, length: 100 };
         let output = format!("{}", h);
         assert_eq!(output.len(), h.length);
