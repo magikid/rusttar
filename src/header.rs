4a07f3d226770a71608f97342bb7630655ebf498
Chris W Jones <chris@christopherjones.us>
2017-03-12 15:03:25 -0400

    Starting on CLI component
    I realized that maybe the data structures would come to me more easily
    if I started writing out a way to interface with them so I've started
    writing the CLI component using getopts.

--- src/header.rs
+++ src/header.rs
@@ -1,6 +1,42 @@
-use name_field::NameField;
+struct Archive {
+    file_entries: Vec<FileEntry>,
+    footer: [u8; 1024]
+}
 
 #[derive(Debug)]
-pub struct Header{
-    name: NameField
+struct FileEntry {
+    header: Header,
+    file: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct StarHeader{
+    name: [u8; 100],
+    mode: [u8; 8],
+    uid: [u8; 8],
+    gid: [u8; 8],
+    size: [u8; 12],
+    mtime: [u8; 12],
+    chksum: [u8; 8],
+    typeflag: [u8; 1],
+    linkname: [u8; 100],
+    magic: [u8; 6],
+    version: [u8; 2],
+    uname: [u8; 32],
+    gname: [u8; 32],
+    devmajor: [u8; 8],
+    devminor: [u8; 8],
+    prefix: [u8; 131],
+    atime: [u8; 12],
+    ctime: [u8; 12]
+}
+
+impl Header for StarHeader{
+    fn as_bytes(&self) {
+        self.name
+    }
+}
+
+trait Header {
+    fn as_bytes(&self);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-mod header;
-mod name_field;
-use header::Header;
+extern crate getopts;
+use getopts::Options;
+use std::env;
 
-trait HeaderField {
-    fn as_bytes(&self) -> Vec<u8>;
-    fn len(&self) -> usize;
-    fn truncate(&self) -> Vec<u8>;
-    fn pad(&self) -> Vec<u8>;
-}
-
-#[derive(Debug)]
-struct Archive {
-    header: Header,
-    file: Vec<u8>
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn main() {
-    println!("hi");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let output = matches.opt_str("o");
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+    do_work(&input, output);
 }
