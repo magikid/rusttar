31e1ff26e3f72a255a52bf083259220e66cba205
Chris W Jones <chris@christopherjones.us>
2017-03-13 18:01:12 -0400

    Got getopts running
    This change introduces getops for parsing the arguemnts.  So far I only
    have a filename argument, a help flag, and a flag specifying that the
    file contents should be listed.

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,31 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optopt("f", "", "the tar archive to read", "NAME");
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("t", "list", "list the archive contents");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-    if matches.opt_present("h") {
+    if matches.opt_present("h") || !matches.opt_present("f"){
         print_usage(&program, opts);
         return;
     }
-    let output = matches.opt_str("o");
-    let input = if !matches.free.is_empty() {
-        matches.free[0].clone()
+
+    let archive = matches.opt_str("f");
+    if matches.opt_present("t") {
+        list(archive);
     } else {
+        println!("You didn't specify an operation!");
         print_usage(&program, opts);
-        return;
+    }
+}
+
+fn list(file_argument: Option<String>){
+    let filename = match file_argument{
+        Some(n) => { n },
+        None => { panic!("Filename problem") }
     };
-    do_work(&input, output);
+    println!("Work, work, work!");
 }
