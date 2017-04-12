3276f4bd32e325e749dcafce65d287a753b7cfa5
Chris W Jones <chris@christopherjones.us>
2017-04-12 17:26:23 -0400

    Refactored code from yesterday
    I moved the Header struct back into its own mod.  I also cleaned up the
    logic for grabbing the headers to use a draining iterator on the bytes
    in the file.  I think this is a more elegant solution for getting the
    bytes than keeping track of the offset myself.

--- src/archive/header.rs
+++ src/archive/header.rs
@@ -5,13 +5,13 @@ use std::str::FromStr;
 
 #[derive(Clone,Debug)]
 pub struct Header{
-    pub file_name: String,
-    pub size: i32
+    file_name: String,
+    size: i32
 }
 
 #[derive(Clone,Debug)]
-pub struct Headers{
-    pub headers: Vec<Header>
+struct Headers{
+    headers: Vec<Header>
 }
 
 impl Iterator for Headers {
@@ -22,7 +22,50 @@ impl Iterator for Headers {
 }
 
 impl Headers {
-    pub fn push(&mut self, elem: Header){
+    fn push(&mut self, elem: Header){
         self.headers.push(elem)
     }
+}
+
+enum HeaderOffsets{
+    name = 0,
+    mode = 100,
+    uid = 108,
+    gid = 116,
+    size = 124,
+    mtime = 135
+}
+
+impl Header {
+    pub fn new(bytes: Vec<u8>) -> Header {
+        let file_name = get_name(bytes[(HeaderOffsets::name as usize)..(HeaderOffsets::mode as usize)].to_vec());
+        let file_size = get_size(bytes[(HeaderOffsets::size as usize)..(HeaderOffsets::mtime as usize)].to_vec());
+        let header = Header { file_name: file_name, size: file_size };
+        header
+    }
+
+    pub fn file_name(&self) -> String {
+        self.file_name.clone()
+    }
+
+    pub fn size(&self) -> i32 {
+        self.size
+    }
+}
+
+fn get_name(bytes: Vec<u8>) -> String{
+    match String::from_utf8(bytes) {
+        Ok(x) => x.trim_right_matches("\0").to_string(),
+        Err(_) => String::from("")
+    }
+}
+
+fn get_size(bytes: Vec<u8>) -> i32{
+    // For some reason, GNU tar writes the file size as a string instead of a number so we
+    // first parse it as a string, then parse the number from the string.
+    let size = match i32::from_str(&String::from_utf8(bytes.clone()).unwrap()) {
+        Ok(x) => x,
+        Err(_) => 0
+    };
+    size
 }
--- src/archive/mod.rs
+++ src/archive/mod.rs
@@ -6,45 +6,36 @@ use archive::header::*;
 use std::str::FromStr;
 
 pub struct Archive {
-    pub file: String,
+    pub file: String
 }
 
 impl Archive {
     pub fn listing(&self) -> Vec<String>{
-        println!("{:?}", self.parse_headers());
-        vec![self.file.clone()]
-    }
-
-    fn parse_headers(&self) -> Headers {
-        let record_size = 512;
-        let mut headers = Headers{ headers: Vec::new() };
         let file_handle = self.open_file();
-        let mut header_bytes = file_handle.bytes()
+        let mut file_names = Vec::new();
+        let mut file_bytes = file_handle.bytes()
             .map(|x| x.unwrap())
             .collect::<Vec<u8>>();
 
         let mut base = 0;
-        println!("base: {:?}, header_bytes: {:?}", base, header_bytes.len());
-        while base + 1024 <= header_bytes.len() {
-            let null_char = "\0";
-            let name_str = match String::from_utf8(header_bytes[base..base+99].to_vec()) {
-                Ok(x) => x,
-                Err(_) => continue
+        while file_bytes.len() > 0 {
+            let first_byte = match file_bytes.first() {
+                Some(x) => x.clone(),
+                None => panic!("No byte?")
             };
-            let name = name_str.trim_right_matches(null_char).clone().to_string();
-            let size = match i32::from_str(&String::from_utf8(header_bytes[base+124..base+135].to_vec()).unwrap()) {
-                Ok(x) => x,
-                Err(_) => record_size
-            };
+            if first_byte != 0u8 {
+                let header_bytes = file_bytes.drain(..512).collect::<Vec<u8>>();
+                let header = Header::new(header_bytes);
+                file_names.push(header.file_name());
 
-            let header = Header { file_name: name.to_string(), size: size };
-            headers.push(header);
-            let skip_files = record_size * ((size as f32 / record_size as f32) as f32).ceil() as i32;
-            base += (record_size + skip_files) as usize;
-            println!("base: {:?}, ceil: {:?}", base, skip_files);
+                let skip_length = calculate_blocks(header.size());
+                file_bytes.drain(..(skip_length * 512) as usize);
+            }else{
+                file_bytes.drain(..512);
+                continue
+            }
         }
-
-        headers
+        file_names
     }
 
     fn open_file(&self) -> File {
@@ -54,3 +45,8 @@ impl Archive {
         }
     }
 }
+
+fn calculate_blocks(size: i32) -> i32 {
+    ((size as f64 / 512f64) as f64).ceil() as i32
+}
+
